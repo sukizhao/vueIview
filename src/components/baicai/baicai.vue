e0d59cbd896becd3531c9e7f24357d48fb057a73
zhaoshuqi <852056893@qq.com>
2019-03-08 14:21:54 +0800

    学习引入css和less

--- src/components/baicai/baicai.vue
+++ src/components/baicai/baicai.vue
@@ -1,30 +1,36 @@
 <template>
   <div>
-    <h2 class="text-center">{{title}}</h2>
-    <ul class="text-center spacing" style="padding: 0">
-      <li v-for="item in links"><a @click="$goRoute(item.route)">{{item.text}}</a></li>
-    </ul>
+    <p class="dabaicai">{{title}}</p>
+    <!-- <ul class="text-center spacing" style="padding: 0">
+      <li v-for="item in links">
+        <a @click="$goRoute(item.route)">{{item.text}}</a>
+      </li>
+    </ul>-->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'page01',
-  data () {
+  name: "page01",
+  data() {
     return {
-      title: '这里是第一个页面',
+      title: "这里是第一个页面",
       links: [
         {
-          text: '圆白菜',
-          route: '/page01/page01-a'
+          text: "圆白菜",
+          route: "/page01/page01-a"
         },
         {
-          text: '长白菜',
-          route: '/page01/page01-b'
+          text: "长白菜",
+          route: "/page01/page01-b"
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
+<style>
+@import "./baicai.css";
+</style>
+
